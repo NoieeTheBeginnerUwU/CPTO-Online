ba76255653d2a1bf0c53e699722deac1d2d4918f
OptiShen <arevalokenth13@gmail.com>
2022-12-02 22:19:13 +0800

    Added MAPS.API for "Where to Go" Added new images Added wtgo.css

--- script/maps/google-map-data.js
+++ script/maps/google-map-data.js
@@ -1,11 +1,11 @@
  $(function() {
     //============================Google maps=============================
     if($("#google_ptm_map").length > 0){
-        var gPTMCords = new google.maps.LatLng(50.43, 30.60);
-        var gPTMOptions = {zoom: 8,center: gPTMCords, mapTypeId: google.maps.MapTypeId.ROADMAP}    
+        var gPTMCords = new google.maps.LatLng(14.0996, 122.9550);
+        var gPTMOptions = {zoom: 9,center: gPTMCords, mapTypeId: google.maps.MapTypeId.ROADMAP}    
         var gPTM = new google.maps.Map(document.getElementById("google_ptm_map"), gPTMOptions);
         
-        var cords = new google.maps.LatLng(50.37, 30.65);
+        var cords = new google.maps.LatLng(14.0996, 122.9550);
         var marker = new google.maps.Marker({position: cords, map: gPTM, title: "Marker 1"});  
         cords = new google.maps.LatLng(50.5, 30.55);
         marker = new google.maps.Marker({position: cords, map: gPTM, title: "Marker 2"});
